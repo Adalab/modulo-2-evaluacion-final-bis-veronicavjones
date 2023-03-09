80e25d1095cc24afd3c729f3336a2fb48f0292cc
veronicavjones <veronicavjones@hotmail.com>
2023-03-09 19:18:31 +0100

    trying to paint listData

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-console.log('>> Ready :)');
+const save = document.querySelector("js-btnSaveData" );
+const recover = document.querySelector(".js-btnRecoverData");
+const list = document.querySelector(".js-listData");
+
+let listData = [];
+
+fetch("https://randomuser.me/api/?results=10")
+
+   .then (response => response.json())
+   .then (data => {
+      console.log(data.results);
+      listData = data.results;
+       //renderList(listData[0]); 
+   })
+
+
+function renderList(results){
+  //list.innerHTML = ''
+  for( data of listData) {
+    let html = `<li>
+        <li class="nombre ${listData}"></li>
+        <li class="ciudad ${listData}" ></li>
+        <li class="foto ${listData}"></li>
+        <li class="nombre de usuario ${listData}"></li>
+    </li>`
+    
+  }
+   results.innerHTML = html;
+} 
+
+
+
+
